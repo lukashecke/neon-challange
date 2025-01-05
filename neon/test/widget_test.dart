72ad668546be6cb0d08fb3c5afbf1faea224a1ff
Lukas Hecke <info@lukashecke.com>
2025-01-05 20:21:41 +0100

    unit tests for button functionallity

--- neon/test/widget_test.dart
+++ neon/test/widget_test.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:neon/feature/age_estimation/bloc/age_estimation_bloc.dart';
 import 'package:neon/feature/age_estimation/data/age_estimation_repository.dart';
 import 'package:neon/feature/age_estimation/model/age_estimation.dart';
+import 'package:neon/main.dart';
 
 class MockRepository extends Mock implements AgeEstimationRepository {}
 
@@ -48,6 +50,151 @@ void main() {
         emitsInOrder([
           isA<AgeEstimationError>(),
         ]),
+      );
+    });
+  });
+
+  group('AgeEstimationPage (User Story 2)', () {
+    late AgeEstimationRepository ageEstimationRepository;
+
+    setUp(() {
+      ageEstimationRepository = MockRepository();
+      when(() => ageEstimationRepository.fetchNameEstimation(any())).thenAnswer(
+        (_) async => AgeEstimation(name: 'Test', age: 42, count: 1),
+      );
+    });
+
+    testWidgets('Button does nothing initially', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+
+      final buttonFinder = find.widgetWithIcon(
+        FloatingActionButton,
+        Icons.search,
+      );
+
+      expect(
+        tester.widget<FloatingActionButton>(buttonFinder).onPressed,
+        isNull,
+      );
+    });
+
+    testWidgets('Button activates when TextField is not empty',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+
+      // Verify that the button is initially disabled
+      final textFieldFinder = find.byType(TextField);
+      final buttonFinder = find.widgetWithIcon(
+        FloatingActionButton,
+        Icons.search,
+      );
+
+      await tester.enterText(textFieldFinder, 'Test');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pump();
+
+      expect(
+        tester.widget<FloatingActionButton>(buttonFinder).onPressed,
+        isNotNull,
+      );
+    });
+
+    testWidgets('Button deactivates after search', (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+
+      // Verify that the button is initially disabled
+      final textFieldFinder = find.byType(TextField);
+      final buttonFinder = find.widgetWithIcon(
+        FloatingActionButton,
+        Icons.search,
+      );
+
+      await tester.enterText(textFieldFinder, 'Test');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pump();
+
+      expect(
+        tester.widget<FloatingActionButton>(buttonFinder).onPressed,
+        isNotNull,
+      );
+
+      await tester.tap(find.byIcon(Icons.search));
+      await tester.pump();
+
+      expect(
+        tester.widget<FloatingActionButton>(buttonFinder).onPressed,
+        isNull,
+      );
+    });
+
+    testWidgets('Button reactivates after searching a new name',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+
+      // Verify that the button is initially disabled
+      final textFieldFinder = find.byType(TextField);
+      final buttonFinder = find.widgetWithIcon(
+        FloatingActionButton,
+        Icons.search,
+      );
+
+      await tester.enterText(textFieldFinder, 'Test');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pump();
+
+      await tester.tap(find.byIcon(Icons.search));
+      await tester.pump();
+
+      expect(
+        tester.widget<FloatingActionButton>(buttonFinder).onPressed,
+        isNull,
+      );
+
+      await tester.enterText(textFieldFinder, 'Test2');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pump();
+
+      expect(
+        tester.widget<FloatingActionButton>(buttonFinder).onPressed,
+        isNotNull,
+      );
+    });
+
+    testWidgets(
+        'Button deactivates after searching the same name twice in a row',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const MyApp());
+
+      // Verify that the button is initially disabled
+      final textFieldFinder = find.byType(TextField);
+      final buttonFinder = find.widgetWithIcon(
+        FloatingActionButton,
+        Icons.search,
+      );
+
+      await tester.enterText(textFieldFinder, 'Test');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pump();
+
+      expect(
+        tester.widget<FloatingActionButton>(buttonFinder).onPressed,
+        isNotNull,
+      );
+
+      await tester.tap(find.byIcon(Icons.search));
+      await tester.pump();
+
+      await tester.enterText(textFieldFinder, '');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pump();
+
+      await tester.enterText(textFieldFinder, 'Test');
+      await tester.testTextInput.receiveAction(TextInputAction.done);
+      await tester.pump();
+
+      expect(
+        tester.widget<FloatingActionButton>(buttonFinder).onPressed,
+        isNull,
       );
     });
   });
